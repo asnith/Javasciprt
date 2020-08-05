1f8d9cba4fdc5489aa38658ff66d4d289f941b49
asnith <69137127+asnith@users.noreply.github.com>
2020-08-05 10:04:16 +0530

    Create java.js

---
+++ java.js
@@ -0,0 +1,9 @@
+var arr = ["akash", "john","joe", "ravi", "rajeev"];
+for (var i=0;i<arr.length;i++) {
+	p = arr[i];
+	if (p[0]=="j" !! p[0]=="J") {
+	console.log("Goodbye " + p);
+	} else {
+	console.log("Hello " + p);
+	}
+}
